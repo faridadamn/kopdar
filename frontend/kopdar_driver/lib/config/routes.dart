47038b8b620f5a390cc94f7c1cb3a9c7063c880f
faridadamn <160025733+faridadamn@users.noreply.github.com>
2026-07-15 20:20:28 +0700

    chore: remove unused router import

--- frontend/kopdar_driver/lib/config/routes.dart
+++ frontend/kopdar_driver/lib/config/routes.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../features/splash/splash_screen.dart';
 import '../features/auth/presentation/pages/phone_input_page.dart';
